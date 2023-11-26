5b2a26894325306befa14d6deb0e38911ba22e02
Scott Halgrim <shalgrim@gmail.com>
2023-11-25 22:02:54 -0800

    stage 6 complete

--- Smart Calculator (Go)/task/main.go
+++ Smart Calculator (Go)/task/main.go
@@ -3,62 +3,141 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"log"
 	"os"
 	"strconv"
+	"strings"
 	"unicode"
 )
 
+func processCommand(command string) {
+	switch command {
+	case "exit":
+		fmt.Println("Bye!")
+		os.Exit(0)
+	case "help":
+		fmt.Println("The program calculates the sum or difference of numbers and keeps track of variable assignments")
+	default:
+		fmt.Println("Unknown command")
+	}
+}
+
+func processAssignment(input string, variables map[string]int) map[string]int {
+	split := strings.Split(input, "=")
+	if len(split) != 2 {
+		fmt.Println("Invalid assignment")
+		return variables
+	}
+	variable := strings.TrimSpace(split[0])
+	sValue := strings.TrimSpace(split[1])
+
+	if isValidVariable(sValue) {
+		val, ok := variables[sValue]
+		if ok {
+			variables[variable] = val
+		} else {
+			fmt.Println("Unknown variable")
+		}
+	} else if !isValidVariable(variable) {
+		fmt.Println("Invalid identifier")
+	} else {
+		iValue, err := strconv.Atoi(sValue)
+		if err != nil {
+			fmt.Println("Invalid assignment")
+		} else {
+			variables[variable] = iValue
+		}
+	}
+	return variables
+}
+
+func isValidVariable(input string) bool {
+	for _, char := range input {
+		if !unicode.IsLetter(char) {
+			return false
+		}
+	}
+	return true
+}
+
+func processVariableExpression(input string, variables map[string]int) {
+	if isValidVariable(input) {
+		val, ok := variables[input]
+		if ok {
+			fmt.Println(val)
+		} else {
+			fmt.Println("Unknown variable")
+		}
+	} else {
+		fmt.Println("Invalid identifier")
+	}
+}
+
+func processExpression(input string, variables map[string]int) {
+	tokens := strings.Split(input, " ")
+	sum := 0
+	sign := 1
+	numOrVarAcceptable := true
+	for _, token := range tokens {
+		if token == "" {
+			continue
+		} else if isValidVariable(token) {
+			if !numOrVarAcceptable {
+				fmt.Println("error A")
+				return
+			}
+			val, ok := variables[token]
+			if !ok {
+				fmt.Println("Unknown variable")
+				return
+			}
+			sum += sign * val
+			sign = 1
+			numOrVarAcceptable = false
+		} else if token == "-" {
+			sign *= -1
+			numOrVarAcceptable = true
+		} else if token == "+" {
+			numOrVarAcceptable = true
+		} else {
+			if !numOrVarAcceptable {
+				fmt.Println("error B")
+				return
+			}
+			val, err := strconv.Atoi(token)
+			if err != nil {
+				fmt.Println("unknown error")
+			}
+			sum += val * sign
+			sign = 1
+			numOrVarAcceptable = false
+		}
+	}
+	fmt.Println(sum)
+}
+
+func faukenizer(input string) []string {
+	myInput := strings.ReplaceAll(input, "+", " + ")
+	myInput = strings.ReplaceAll(myInput, "-", " - ")
+	return strings.Split(myInput, " ")
+}
+
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
+	variables := make(map[string]int)
 
 	for {
-		sum := 0
-		sign := 1
-		currentNumber := 0
-		signNeedsResetting := false
 		scanner.Scan()
 		input := scanner.Text()
 
 		if input == "" {
 			continue
-		} else if input == "/exit" {
-			fmt.Println("Bye!")
-			os.Exit(0)
-		} else if input == "/help" {
-			fmt.Println("The program calculates the sum or difference of numbers")
+		} else if string(input[0]) == "/" {
+			processCommand(input[1:])
+		} else if strings.Contains(input, "=") {
+			variables = processAssignment(strings.TrimSpace(input), variables)
 		} else {
-			for _, char := range input {
-				if unicode.IsSpace(char) || unicode.IsControl(char) {
-					sum += currentNumber * sign
-					currentNumber = 0
-					if signNeedsResetting {
-						sign = 1
-					}
-				} else if unicode.IsNumber(char) {
-					signNeedsResetting = true
-					thisInt, err := strconv.Atoi(string(char))
-					if err != nil {
-						log.Fatal(err)
-					}
-					currentNumber *= 10
-					currentNumber += thisInt
-				} else {
-					// must be either + or _
-					sum += currentNumber * sign
-					if signNeedsResetting {
-						sign = 1
-						signNeedsResetting = false
-					}
-					currentNumber = 0
-					if char == '-' {
-						sign *= -1
-					}
-					currentNumber = 0
-				}
-			}
-			sum += currentNumber * sign
-			fmt.Println(sum)
+			tokens := faukenizer(input)
+			processExpression(strings.Join(tokens, " "), variables)
 		}
 	}
 }
